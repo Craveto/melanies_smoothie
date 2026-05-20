6395c229a4662a562e101b2aa1d89b806ef228b5
Snehal Dighore <snehaldighore89@gmail.com>
2026-05-20 12:15:52 +0530

    Refactor Snowflake session handling in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
@@ -14,9 +14,10 @@ name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name will be", name_on_order)
 
 
-from snowflake.snowpark.functions import col
+cnx =st.connection("snowflake")
+session = cnx.session()
+# session = get_active_session()
 
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 # st.dataframe(data=my_dataframe, use_container_width=True) 
 
